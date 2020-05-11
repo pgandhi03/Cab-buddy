a5a996d1ce33adf8a316fb0d9e4a5d781800a22b
Prachi Gandhi <44058529+pgandhi03@users.noreply.github.com>
2020-05-11 13:39:40 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -16,15 +16,15 @@ mail=Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'your mail address'
-app.config['MAIL_PASSWORD'] = 'your mail password'
+app.config['MAIL_USERNAME'] = '<your mail address>'
+app.config['MAIL_PASSWORD'] = '<your mail password>'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 
 def maill(recp_list,final_msg):
-	msg = Message('From easycabpooling', sender ='easycabpooling@gmail.com' , recipients = recp_list)
+	msg = Message('From easycabpooling', sender ='<your mail address>' , recipients = recp_list)
 	msg.body = final_msg
 	mail.send(msg)
 	return "Sent"
