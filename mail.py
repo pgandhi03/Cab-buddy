602d8ea68ecbc53045fd9e9c0baac9d35a986463
Prachi Gandhi <44058529+pgandhi03@users.noreply.github.com>
2020-05-11 13:39:57 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,16 +1,6 @@
 from flask import Flask
 from flask_mail import Mail, Message
 
-
-
-#import urllib2
-
-# proxy_support = urllib2.ProxyHandler({"http":"http://61.233.25.166:80"})
-# opener = urllib2.build_opener(proxy_support)
-# urllib2.install_opener(opener)
-
-# html = urllib2.urlopen("http://www.google.com").read()
-
 app =Flask(__name__)
 mail=Mail(app)
 
