5a522b01e478385d6f6e922837c8bb6e51f9e706
Prachi Gandhi <44058529+pgandhi03@users.noreply.github.com>
2020-05-17 14:31:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,15 +24,10 @@ def index():
 @app.route('/about')
 def about():
 	
-
-
 	return render_template("about.html")
 
 @app.route('/contact')
 def contact():
-	
-
-
 	return render_template("contact.html")
 
 
